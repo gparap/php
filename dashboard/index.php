b5f876a20784d43e14405e37c14a8067e67cdb3a
gparap <gparap@protonmail.com>
2022-09-25 16:06:11 +0200

    Initial commit (dashboard)

---
+++ dashboard/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dashboard</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+    <!--bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" ></script>
+</head>
+
+<body>
+    <!-- Logo -->
+    <div class="container">
+        <div class="col-md-12">
+            <img src="img/gparap_logo.png" width="128px" height="64px" alt="logo">
+        </div>
+    </div>
+</body>
+
+</html>
