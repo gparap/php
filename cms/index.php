f68d4a19ccb409f016da57b42ace8ef1f2a225dc
gparap <gparap@protonmail.com>
2022-10-05 10:04:37 +0200

    Initial commit (content management system)

---
+++ cms/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CMS</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Content Management System</h1>
+    </div>
+</body>
+
+</html>
