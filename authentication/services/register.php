6dbf642fbd8ee5a8526c6ef4c57bc9f1b638f856
gparap <gparap@protonmail.com>
2023-01-09 15:25:22 +0200

    Create registration web service

---
+++ authentication/services/register.php
@@ -0,0 +1,40 @@
+<?php
+if (!empty($_POST['email']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+    $result = array();
+
+    //get POST values
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //connect to the database
+    require_once('../src/connection.php');
+
+    if (!$db_connection) {
+    	$result = array("status"=>"0", "msg"=>"Database connection failed.");
+    }else{
+        //check if user already exists
+        $query = "SELECT * FROM `users` WHERE email='$email'";
+        $query_result = mysqli_query($db_connection, $query);
+        $query_result_rows = mysqli_num_rows($query_result);
+        if ($query_result_rows == 1) {
+            $result = array("status"=>"0", "msg"=>"User already registered.");
+        }
+        else{
+            //register user
+            $query = "INSERT INTO `users`(`email`, `username`, `password`) VALUES('$email', '$username', '$password')";
+            $query_result = mysqli_query($db_connection, $query);
+
+            //check user registration result
+            if ($query_result == 1) {
+                $result = array("status"=>"1", "msg"=>"User registration successful.");
+            }else{
+                $result = array("status"=>"0", "msg"=>"User registration failed.");
+            }
+        }
+    }
+
+    //create json response
+    echo json_encode($result);
+}
+?>
