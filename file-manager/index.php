46b496ce5f32d69bbe321b38b3434587cdd2ad9d
gparap <gparap@protonmail.com>
2023-01-19 16:39:45 +0200

    Initial commit (File Manager)

---
+++ file-manager/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+https://mit-license.org
+Copyright © 2023 gparap
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>File Manager</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <h1>File Manager</h1>        
+        <script src="js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
