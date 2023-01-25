673369fc6a5c98662a21176abe9644d5214fd270
gparap <gparap@protonmail.com>
2023-01-25 19:59:31 +0200

    Initial commit (E-Commerce)

---
+++ e-commerce/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * https://mit-license.org
+ * Copyright © 2023 gparap
+ * Database configuration.
+ */
+
+ define('HOST', 'localhost');
+ define('DATABASE', 'e_commerce_db');
+ define('USER', 'root');
+ define('PASSWORD', '');
+
+?>
---
+++ e-commerce/public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+https://mit-license.org
+Copyright © 2023 gparap
+-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce</title>
+</head>
+<body>
+    <?php
+        echo("info: Project is currently used as backend to android app \"e-commerce\" in android repo.");
+    ?>
+</body>
+</html>
