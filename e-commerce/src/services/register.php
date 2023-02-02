d8038654450c69a2ae2c4eb1b8b48e5d9853bb2d
gparap <gparap@protonmail.com>
2023-02-02 10:15:03 +0200

    Update registration service to handle existing users

--- e-commerce/src/services/register.php
+++ e-commerce/src/services/register.php
@@ -6,36 +6,41 @@
  * Register user into the database.
  */
 
-if (!empty($_POST['username']) && !empty($_POST['email'])) {    //TODO: password hash
+if (!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['email'])) {
     $json_response = array();
 
     //user registration fields
     $email = $_POST['email'];
     $username = $_POST['username'];
-    $password = $_POST['password'];
+    $password = hash('md5', $_POST['password']);
     $address = $_POST['address'];
     $postal_code = $_POST['postal_code'];
     $phone = $_POST['phone'];
 
-    //TODO: check if user already exists
-
-    //register user into the database
+    //register user unless they already exist in the database
     require_once('../utils/connection.php');
-    $query = "INSERT INTO `users` (`email`, `username`, `password`, `address`, `postal_code`, `phone`) 
-                    VALUES ('$email', '$username', '$password', '$address', '$postal_code', '$phone')";
-    $query_result = mysqli_query($db_connection, $query);
+    $query_email = "SELECT * FROM `users` WHERE email='$email'";
+    $query_email_result = mysqli_query($db_connection, $query_email);
+    if (mysqli_num_rows($query_email_result) == 1) {
+        //user already registered
+        $json_response = array("status" => "0", "msg" => "User is already registered in the database.", "user" => $username);
 
-    if ($query_result == true) {
-        //user registerd ok
-        $json_response = array("status"=>"1", "msg"=>"User registration succeeded.",
-        "query_result"=>$query_result);
-    }else{
-        //user couldn't register
-        $json_response = array("status"=>"0", "msg"=>"User registration failed.");
+    } else {
+        //register user into the database
+        $query = "INSERT INTO `users` (`email`, `username`, `password`, `address`, `postal_code`, `phone`) 
+                        VALUES ('$email', '$username', '$password', '$address', '$postal_code', '$phone')";
+        $query_result = mysqli_query($db_connection, $query);
+
+        if ($query_result == true) {
+            //user registerd ok
+            $json_response = array("status" => "1", "msg" => "User registration succeeded.", "user" => $username);
+        } else {
+            //user couldn't register
+            $json_response = array("status" => "0", "msg" => "User registration failed.", "user" => "");
+        }
     }
 
     //generate json response
     echo json_encode($json_response);
 }
 
-?>
