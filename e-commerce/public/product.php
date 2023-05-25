095970ad923aa6cb1ffbdb0df54b821d77dd889b
gparap <gparap@protonmail.com>
2023-05-25 22:06:41 +0300

    Apply an e-commerce template

---
+++ e-commerce/public/product.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Product - E-Commerce</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+    <?php include_once('../src/utils/navigation.php'); ?>
+    <section class="py-5">
+        <div class="container py-5">
+            <div><!-- TODO:Product --></div>
+        </div>
+    </section>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- e-commerce/public/shopping-cart.php
+++ e-commerce/public/shopping-cart.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Products - E-Commerce</title>
+    <title>Shopping Cart - E-Commerce</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
@@ -12,21 +12,20 @@
     <?php include_once('../src/utils/navigation.php'); ?>
     <section class="py-5">
         <div class="container py-5">
-            <div class="row mb-4 mb-lg-5">
+             <div class="row mb-4 mb-lg-5">
                 <div class="col-md-8 col-xl-6 text-center mx-auto">
-                    <h2 class="fw-bold">Products</h2>
-                    <p class="text-muted">We hope you have a nice shopping experience.&nbsp;</p>
+                    <h2 class="fw-bold">Shopping Cart</h2>
+                    <p class="text-muted">Review your items before checking out.&nbsp;</p>
                 </div>
             </div>
             <div class="row mx-auto">
                 <div class="col">
-                    <div><!-- TODO:Products --></div>
+                    <div><!-- TODO:Shopping Cart --></div>
                 </div>
             </div>
         </div>
     </section>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/bs-init.js"></script>
 </body>
 
 </html>
