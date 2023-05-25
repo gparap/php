31b68bddd858c25688af3793482f885046b27452
gparap <gparap@protonmail.com>
2023-05-25 14:38:32 +0300

    Remove abandoned projects

--- dashboard/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// connect to database
-$db_connection = mysqli_connect('localhost', 'root', '', 'test_db');
-if (!$db_connection) {
-    die(mysqli_connect_error());
-}
--- dashboard/js/admin.js
+++
@@ -1,5 +0,0 @@
-function displayCategoryContents(id) {
-    let category = document.getElementById(id);
-    let content = document.getElementById('dashboard-category-content');
-    content.innerHTML = category.id;
-}
