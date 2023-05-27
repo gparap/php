1b9c1222728e28d089bdb26ba402665d1dc8e4f4
gparap <gparap@protonmail.com>
2023-05-27 10:26:09 +0300

    Create basic pagination for displaying the products

--- e-commerce/public/index.php
+++ e-commerce/public/index.php
@@ -12,20 +12,60 @@
     <?php include_once('../src/utils/navigation.php'); ?>
     <section class="py-5">
         <div class="container py-5">
+            <!-- Header -->
             <div class="row mb-4 mb-lg-5">
                 <div class="col-md-8 col-xl-6 text-center mx-auto">
                     <h2 class="fw-bold">Products</h2>
                     <p class="text-muted">We hope you have a nice shopping experience.&nbsp;</p>
                 </div>
             </div>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <?php
 
-                require_once('../src/utils/connection.php');
-                require_once('../src/utils/functions.php');
+            <?php
+            require_once('../src/utils/connection.php');
+
+            //query the database for the total number of products
+            $query_products_count = "SELECT COUNT(*) as count FROM `products`";
+            $products_count_result = mysqli_query($db_connection, $query_products_count);
+            $products_count_assoc = mysqli_fetch_assoc($products_count_result);
+            $products_count = $products_count_assoc['count'];
+
+            //get the current page
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'];
+            } else {
+                $page = 1;
+            }
+
+            //setup pagination limits
+            $page_limit = 6;
+            $pages = $products_count / $page_limit;
+            ?>
+
+            <!-- Pagination -->
+            <nav>
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled"><a class="page-link">Pages</a></li>
+                    <?php
+                    for ($i = 1; $i <= $pages; $i++) {
+                        echo '
+                            <li class="page-item"><a class="page-link" 
+                            href="https://localhost/e-commerce/public/index.php?page=' . $i . '">' . $i . '</a></li>
+                            ';
+                    }
+                    ?>
+                </ul>
+            </nav>
+
+            <!-- Display Products -->
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <?php require_once('../src/utils/functions.php');
+
+                //get pagination limits
+                $pagination_start = ($page * $page_limit) - ($page_limit - 1);
+                $pagination_end = $page * $page_limit;
 
                 //query the database for products
-                $query_products = "SELECT * FROM `products`";
+                $query_products = "SELECT * FROM `products` WHERE id >= " . $pagination_start . " AND id <=" . $pagination_end;
                 $query_products_results = mysqli_query($db_connection, $query_products);
                 if (!$query_products_results) {
                     exit("Cannot connect to database.");
@@ -70,7 +110,6 @@
 
                 //close database connection
                 $db_connection->close();
-
                 ?>
             </div>
         </div>
