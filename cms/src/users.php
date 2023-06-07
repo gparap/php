58acd872ce4b5ce1b3f8820635dc43cde5d253f1
gparap <gparap@protonmail.com>
2023-06-07 18:12:42 +0300

    Fetch & Display all users

--- cms/src/users.php
+++ cms/src/users.php
@@ -25,8 +25,7 @@ if (empty($_SESSION['user_id'])) {
 
             <!--Content Area-->
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Users</h1>
                     <h6>
                         <?php if (!empty($_SESSION['user_id'])) {
@@ -36,6 +35,51 @@ if (empty($_SESSION['user_id'])) {
                         } ?>
                     </h6>
                 </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //connect to database
+                        $connection = mysqli_connect('localhost', 'root', '', 'test_db');
+                        if (!$connection) {
+                            die(mysqli_connect_error);
+                        }
+
+                        //get all users
+                        $query = "SELECT * FROM users";
+                        $results = mysqli_query($connection, $query);
+
+
+                        //display all users
+                        while ($row = mysqli_fetch_assoc($results)) {
+                            echo '<tr>';
+                            echo '<th scope="row">' . $row['id'] . '</th>';
+                            echo '<td>' . $row['username'] . '</td>';
+                            echo '<td>' . $row['email'] . '</td>';
+                            echo '<td>' . $row['role'] . '</td>';
+                            echo '<td>' . $row['status'] . '</td>';
+                            echo '<td>';
+                            echo '<a href="user_status.php?id=' . $row["id"] . '"><button name="button-approve" type="button" class="btn btn-success" >APPROVE</button></a>';
+                            echo '<a href="user_status.php?id=' . $row["id"] . '"><button name="button-disapprove" type="button" class="btn btn-warning">DISAPPROVE</button></a>';
+                            echo '<a href="user_delete.php?id=' . $row["id"] . '"><button name="button-delete" type="button" class="btn btn-danger">DELETE</button></a>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+
+                        //!!! important
+                        $connection->close();
+                        ?>
+                    </tbody>
+                </table>
             </main>
         </div>
     </div>
