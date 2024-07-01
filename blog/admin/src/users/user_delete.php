03a7a7a5849ea68d4152d713ccd05f3d01c32cce
gparap <gparap@protonmail.com>
2024-07-01 19:55:55 +0300

    Update the blog admin functionality to delete existing users

--- blog/admin/src/users/user_delete.php
+++ blog/admin/src/users/user_delete.php
@@ -4,5 +4,23 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/blog/config/config.php');
 require_once('../utils/functions');
 checkUserAuthentication();
 
-echo ("TODO: Not implemented yet.");
-?>
+//connect to database
+$connection = connectToDatabase();
+
+//get the user id to be deleted
+$id = $_GET['id'];
+
+//delete user
+$sql = "DELETE FROM users WHERE id='$id' AND role != 'admin'";
+$result = mysqli_query($connection, $sql);
+$affected_rows = $connection->affected_rows;
+if ($result && $affected_rows == 0) {
+    echo '<script>alert("Cannot delete admin!");</script>';
+}
+
+//redirect to users
+$location = ADMIN_URL . "/src/users/users.php";
+echo '<script>window.location.href = "' . $location . '";</script>';
+
+//!!! important
+$connection->close();
