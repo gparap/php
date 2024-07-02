c5c8623bf4f3348ad052840514f0c9e939b70f63
gparap <54770777+gparap@users.noreply.github.com>
2024-07-02 12:39:06 +0300

    Update the blog admin functionality to approve pending users status

--- blog/admin/src/users/user_approve.php
+++ blog/admin/src/users/user_approve.php
@@ -4,5 +4,19 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/blog/config/config.php');
 require_once('../utils/functions');
 checkUserAuthentication();
 
-echo ("TODO: Not implemented yet.");
-?>
+//connect to database
+$connection = connectToDatabase();
+
+//get the user id to be approved
+$id = $_GET['id'];
+
+//approve user
+$query = "UPDATE `users` SET `status`='approved' WHERE id='$id'";
+$result = mysqli_query($connection, $query);
+
+//redirect to users
+$location = ADMIN_URL . "/src/users/users.php";
+echo '<script>window.location.href = "' . $location . '";</script>';
+
+//!!! important
+$connection->close();
